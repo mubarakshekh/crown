581151818d7cadf6d2c0a014326665a94a05f67f
heymubarak <mubarak@keubik.com>
2020-02-21 16:24:17 +0530

    sign up and sign in done with fire base

--- src/components/signIn/signIn.jsx
+++ src/components/signIn/signIn.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import FormInput from '../form-input/form-input';
 import CustomButton from '../custom-button/custom-button';
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 
 import './signIn.scss';
 
@@ -17,10 +17,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    this.setState({ email: '', password: '' });
+    const { email, password } = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: '', password: '' });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handleChange = event => {
@@ -28,7 +35,6 @@ class SignIn extends React.Component {
 
     this.setState({ [name]: value });
   };
-
   render() {
     return (
       <div className='sign-in'>
